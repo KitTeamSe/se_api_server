279cc5589b27ba74b6edb1cb870ea0c84564544a
dldhk97 <dldhk97@gmail.com>
2021-04-04 20:20:38 +0900

    TimeTable Update Dto 작성

--- src/main/java/com/se/apiserver/v1/timetable/infra/dto/TimeTableUpdateDto.java
+++ src/main/java/com/se/apiserver/v1/timetable/infra/dto/TimeTableUpdateDto.java
@@ -1,5 +1,35 @@
 package com.se.apiserver.v1.timetable.infra.dto;
 
+import com.se.apiserver.v1.timetable.domain.entity.TimeTableStatus;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 public class TimeTableUpdateDto {
 
+  @Data
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Builder
+  @ApiModel("시간표 수정 요청")
+  static public class Request{
+
+    @ApiModelProperty(notes = "시간표 id", example = "1")
+    private Long timeTableId;
+
+    @ApiModelProperty(notes = "변경할 이름", example = "테스트 시간표 1")
+    private String name;
+
+    @ApiModelProperty(notes = "변경할 년도", example = "2021")
+    private Integer year;
+
+    @ApiModelProperty(notes = "변경할 학기", example = "2")
+    private Integer semester;
+
+    @ApiModelProperty(notes = "변경할 상태", example = "CREATED")
+    private TimeTableStatus status;
+  }
 }
