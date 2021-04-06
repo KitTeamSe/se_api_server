75a27a4b4211e6cf24ca27b308d92d55c6299eb2
dldhk97 <dldhk97@gmail.com>
2021-04-06 21:05:14 +0900

    사용 가능 강의실 Read Service 작성.

--- src/main/java/com/se/apiserver/v1/usablelectureroom/application/service/UsableLectureRoomReadService.java
+++ src/main/java/com/se/apiserver/v1/usablelectureroom/application/service/UsableLectureRoomReadService.java
@@ -1,5 +1,50 @@
 package com.se.apiserver.v1.usablelectureroom.application.service;
 
+import com.se.apiserver.v1.common.domain.exception.BusinessException;
+import com.se.apiserver.v1.timetable.application.error.TimeTableErrorCode;
+import com.se.apiserver.v1.timetable.domain.entity.TimeTable;
+import com.se.apiserver.v1.timetable.infra.repository.TimeTableJpaRepository;
+import com.se.apiserver.v1.usablelectureroom.application.dto.UsableLectureRoomReadDto;
+import com.se.apiserver.v1.usablelectureroom.application.dto.UsableLectureRoomReadDto.Response;
+import com.se.apiserver.v1.usablelectureroom.application.error.UsableLectureRoomErrorCode;
+import com.se.apiserver.v1.usablelectureroom.domain.entity.UsableLectureRoom;
+import com.se.apiserver.v1.usablelectureroom.infra.repository.UsableLectureRoomJpaRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class UsableLectureRoomReadService {
 
+  private final UsableLectureRoomJpaRepository usableLectureRoomJpaRepository;
+  private final TimeTableJpaRepository timeTableJpaRepository;
+
+  public UsableLectureRoomReadDto.Response read(Long id){
+    UsableLectureRoom usableLectureRoom = usableLectureRoomJpaRepository
+        .findById(id)
+        .orElseThrow(() -> new BusinessException(UsableLectureRoomErrorCode.NO_SUCH_USABLE_LECTURE_ROOM));
+    return UsableLectureRoomReadDto.Response.fromEntity(usableLectureRoom);
+  }
+
+  public PageImpl readAllByTimeTableId(Pageable pageable, Long timeTableId){
+
+    TimeTable timeTable = timeTableJpaRepository
+        .findById(timeTableId)
+        .orElseThrow(() -> new BusinessException(TimeTableErrorCode.NO_SUCH_TIME_TABLE));
+
+    Page<UsableLectureRoom> all = usableLectureRoomJpaRepository
+        .findAllByTimeTable(pageable, timeTable);
+    List<Response> responseList = all
+        .stream()
+        .map(t -> UsableLectureRoomReadDto.Response.fromEntity(t))
+        .collect(Collectors.toList());
+    return new PageImpl(responseList, all.getPageable(), all.getTotalElements());
+  }
 }
--- src/main/java/com/se/apiserver/v1/usablelectureroom/infra/api/UsableLectureRoomApiController.java
+++ src/main/java/com/se/apiserver/v1/usablelectureroom/infra/api/UsableLectureRoomApiController.java
@@ -1,16 +1,22 @@
 package com.se.apiserver.v1.usablelectureroom.infra.api;
 
+import com.se.apiserver.v1.common.infra.dto.PageRequest;
 import com.se.apiserver.v1.common.infra.dto.SuccessResponse;
 import com.se.apiserver.v1.usablelectureroom.application.dto.UsableLectureRoomCreateDto;
+import com.se.apiserver.v1.usablelectureroom.application.dto.UsableLectureRoomReadDto;
+import com.se.apiserver.v1.usablelectureroom.application.dto.UsableLectureRoomReadDto.Response;
 import com.se.apiserver.v1.usablelectureroom.application.service.UsableLectureRoomCreateService;
 import com.se.apiserver.v1.usablelectureroom.application.service.UsableLectureRoomDeleteService;
 import com.se.apiserver.v1.usablelectureroom.application.service.UsableLectureRoomReadService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsableLectureRoomApiController {
   
   private final UsableLectureRoomCreateService usableLectureRoomCreateService;
-//  private final UsableLectureRoomReadService usableLectureRoomReadService;
+  private final UsableLectureRoomReadService usableLectureRoomReadService;
 //  private final UsableLectureRoomDeleteService usableLectureRoomDeleteService;
 
   @PreAuthorize("hasAnyAuthority('SCHEDULE_MANAGE')")
@@ -34,6 +40,22 @@ public class UsableLectureRoomApiController {
   public SuccessResponse<Long> create(@RequestBody @Validated UsableLectureRoomCreateDto.Request request){
     return new SuccessResponse<>(HttpStatus.CREATED.value(), "사용 가능 강의실 생성에 성공했습니다.", usableLectureRoomCreateService
         .create(request));
+  }
+
+  @PreAuthorize("hasAnyAuthority('SCHEDULE_MANAGE')")
+  @GetMapping(path = "/usable-lecture-room/{id}")
+  @ApiOperation("사용 가능 강의실 조회")
+  @ResponseStatus(value = HttpStatus.OK)
+  public SuccessResponse<UsableLectureRoomReadDto.Response> read(@PathVariable(value = "id") Long id){
+    return new SuccessResponse<>(HttpStatus.OK.value(), "성공적으로 조회되었습니다.", usableLectureRoomReadService.read(id));
+  }
+
+  @PreAuthorize("hasAnyAuthority('SCHEDULE_MANAGE')")
+  @GetMapping(path = "/usable-lecture-room")
+  @ApiOperation("시간표에 추가된 모든 사용 가능 강의실 조회")
+  @ResponseStatus(value = HttpStatus.OK)
+  public SuccessResponse<PageImpl<Response>> readAll(@Validated PageRequest pageRequest, Long timeTableId){
+    return new SuccessResponse<>(HttpStatus.OK.value(), "성공적으로 조회되었습니다.", usableLectureRoomReadService.readAllByTimeTableId(pageRequest.of(), timeTableId));
   }
 
 }
