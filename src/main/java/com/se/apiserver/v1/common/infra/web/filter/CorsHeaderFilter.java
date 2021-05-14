655d62d0ad3c19a3d90c08fdbe1877f18f96e7c2
dldhk97 <dldhk97@gmail.com>
2021-05-14 15:13:28 +0900

    사용자 및 관리자 Web 서버 주소를 외부에서 주입하도록 설정

--- src/main/java/com/se/apiserver/v1/common/infra/web/filter/CorsHeaderFilter.java
+++ src/main/java/com/se/apiserver/v1/common/infra/web/filter/CorsHeaderFilter.java
@@ -1,6 +1,6 @@
-package com.se.apiserver.v1.common.infra.web;
+package com.se.apiserver.v1.common.infra.web.filter;
 
-import javax.servlet.FilterRegistration;
+import com.se.apiserver.v1.common.infra.web.config.CorsHeaderProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +10,14 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class  CorsHeaderFilter {
+
+  private final String MANAGER_SERVER_DOMAIN;
+  private final String CLIENT_SERVER_DOMAIN;
+
+  public CorsHeaderFilter(CorsHeaderProperties properties){
+    MANAGER_SERVER_DOMAIN = properties.getManagerServerDomain();
+    CLIENT_SERVER_DOMAIN = properties.getClientServerDomain();
+  }
 
   @Bean
   public FilterRegistrationBean corsFilter(){
