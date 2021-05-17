4b6d07f00892a7e6e78920c5f53650b149b930f8
dldhk97 <dldhk97@gmail.com>
2021-05-18 01:34:08 +0900

    init.sql과 init_lower.sql 통일 자유게시판 생성 SQL 추가 모든 사용자가 사용 권한 가지게 하였음.

--- src/main/resources/init_lower.sql
+++ src/main/resources/init_lower.sql
@@ -1,6 +1,6 @@
 insert into authority (authority_id, name_eng, name_kor) values (1,'ACCOUNT_ACCESS','계정_접근');
 insert into authority (authority_id, name_eng, name_kor) values (2,'ACCOUNT_MANAGE','계정_관리');
-insert into authority (authority_id, name_eng, name_kor) values (3,'BOARD_MANAGE','게시판_관리');
+insert into authority (authority_id, name_eng, name_kor) values (3,'MENU_MANAGE','메뉴_관리');
 insert into authority (authority_id, name_eng, name_kor) values (4,'FILE_ACCESS','파일_접근');
 insert into authority (authority_id, name_eng, name_kor) values (5,'FILE_MANAGE','파일_관리');
 insert into authority (authority_id, name_eng, name_kor) values (6,'TAG_ACCESS','태그_접근');
@@ -16,7 +16,6 @@ insert into authority (authority_id, name_eng, name_kor) values (15,'REPORT_ACCE
 insert into authority (authority_id, name_eng, name_kor) values (16,'REPORT_MANAGE','신고_관리');
 insert into authority (authority_id, name_eng, name_kor) values (17,'AUTHORITY_MANAGE','권한_관리');
 insert into authority (authority_id, name_eng, name_kor) values (18,'SCHEDULE_MANAGE','시간표_관리');
-insert into authority (authority_id, name_eng, name_kor) values (19,'MENU_MANAGE','메뉴_관리');
 
 
 insert into authority_group (authority_group_id, description, name, type) values (1,'비로그인 사용자','DEFAULT', 'ANONYMOUS');
@@ -76,7 +75,6 @@ insert into authority_group_authority_mapping (authority_group_authority_mapping
 insert into authority_group_authority_mapping (authority_group_authority_mapping_id, authority_id, authority_group_id) values (24,16,3);
 insert into authority_group_authority_mapping (authority_group_authority_mapping_id, authority_id, authority_group_id) values (25,17,3);
 insert into authority_group_authority_mapping (authority_group_authority_mapping_id, authority_id, authority_group_id) values (26,18,3);
-insert into authority_group_authority_mapping (authority_group_authority_mapping_id, authority_id, authority_group_id) values (27,19,3);
 
 insert into period (period_id, period_order, name, start_time, end_time) values (1, 1, '1', '09:00:00', '09:50:00');
 insert into period (period_id, period_order, name, start_time, end_time) values (2, 2, '2', '10:00:00', '10:50:00');
@@ -92,3 +90,10 @@ insert into period (period_id, period_order, name, start_time, end_time) values 
 insert into period (period_id, period_order, name, start_time, end_time) values (12, 12, 'C', '19:50:00', '20:35:00');
 insert into period (period_id, period_order, name, start_time, end_time) values (13, 13, 'D', '20:45:00', '21:30:00');
 insert into period (period_id, period_order, name, start_time, end_time) values (14, 14, 'E', '21:40:00', '22:25:00');
+
+insert into authority (authority_id, name_eng, name_kor) values (20, 'freeboard', '자유게시판');
+insert into menu (menu_id, description, menu_order, menu_type, name_eng, name_kor, url, authority_id) values (1, '자유게시판', 1, 'BOARD', 'freeboard', '자유게시판', 'freeboard', 20);
+insert into board (board_id, name_eng, name_kor, menu_id) values (1, 'freeboard', '자유게시판', 1);
+insert into authority_group_authority_mapping (authority_group_authority_mapping_id, authority_id, authority_group_id) values (28,20,1);
+insert into authority_group_authority_mapping (authority_group_authority_mapping_id, authority_id, authority_group_id) values (29,20,2);
+insert into authority_group_authority_mapping (authority_group_authority_mapping_id, authority_id, authority_group_id) values (30,20,3);
