717eb17a9eea1c4e1fa40965ca1c15aaafca1377
dldhk97 <dldhk97@gmail.com>
2021-05-19 17:15:29 +0900

    Swagger 3(OpenAPI 3.0)으로 버전 업

--- src/main/java/com/se/apiserver/v1/common/infra/swagger/config/Swagger2Config.java
+++ src/main/java/com/se/apiserver/v1/common/infra/swagger/config/Swagger2Config.java
@@ -70,8 +70,8 @@ public class Swagger2Config extends WebMvcConfigurationSupport {
 
   @Override
   protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("swagger-ui.html")
-        .addResourceLocations("classpath:/META-INF/resources/");
+    registry.addResourceHandler("/swagger-ui/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
     registry.addResourceHandler("/webjars/**")
         .addResourceLocations("classpath:/META-INF/resources/webjars/");
   }
