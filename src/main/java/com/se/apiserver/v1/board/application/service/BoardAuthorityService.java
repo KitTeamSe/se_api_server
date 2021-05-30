8bb91567ce6473aaa3a959f1c6edfa182e8f8848
dldhk97 <dldhk97@gmail.com>
2021-05-31 03:13:36 +0900

    게시판 접근/관리 권한 검증 API 작성

---
+++ src/main/java/com/se/apiserver/v1/board/application/service/BoardAuthorityService.java
@@ -0,0 +1,34 @@
+package com.se.apiserver.v1.board.application.service;
+
+import com.se.apiserver.v1.account.application.service.AccountContextService;
+import com.se.apiserver.v1.board.application.error.BoardErrorCode;
+import com.se.apiserver.v1.board.domain.entity.Board;
+import com.se.apiserver.v1.board.infra.repository.BoardJpaRepository;
+import com.se.apiserver.v1.common.domain.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BoardAuthorityService {
+
+  private final BoardJpaRepository boardJpaRepository;
+  private final AccountContextService accountContextService;
+
+  public void validateAccessAuthority(Long boardId) {
+    Board board = boardJpaRepository.findById(boardId).orElseThrow(() -> new BusinessException(
+        BoardErrorCode.NO_SUCH_BOARD));
+
+    board.validateAccessAuthority(accountContextService.getContextAuthorities());
+  }
+
+  public void validateManageAuthority(Long boardId) {
+    Board board = boardJpaRepository.findById(boardId).orElseThrow(() -> new BusinessException(
+        BoardErrorCode.NO_SUCH_BOARD));
+
+    board.validateManageAuthority(accountContextService.getContextAuthorities());
+  }
+
+}
