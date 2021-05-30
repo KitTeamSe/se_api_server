f75cfa8c272748dcfc3f9afd168d2ab48c1f1dff
dldhk97 <dldhk97@gmail.com>
2021-05-31 02:14:04 +0900

    권한 그룹을 pk로 조회하면 그 그룹이 가진 권한을 함께 반환하도록 하였음.

--- src/main/java/com/se/apiserver/v1/authority/application/dto/authoritygroup/AuthorityGroupReadDto.java
+++ src/main/java/com/se/apiserver/v1/authority/application/dto/authoritygroup/AuthorityGroupReadDto.java
@@ -1,13 +1,40 @@
 package com.se.apiserver.v1.authority.application.dto.authoritygroup;
 
+import com.se.apiserver.v1.authority.application.dto.authority.AuthorityReadDto;
+import com.se.apiserver.v1.authority.domain.entity.Authority;
 import com.se.apiserver.v1.authority.domain.entity.AuthorityGroup;
 import com.se.apiserver.v1.authority.domain.entity.AuthorityGroupType;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 public class AuthorityGroupReadDto {
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    static public class AuthorityGroupListItem{
+        private Long authorityGroupId;
+
+        private String name;
+
+        private String description;
+
+        private AuthorityGroupType type;
+
+        public static AuthorityGroupListItem fromEntity(AuthorityGroup authorityGroup){
+            return AuthorityGroupListItem.builder()
+                .authorityGroupId(authorityGroup.getAuthorityGroupId())
+                .name(authorityGroup.getName())
+                .description(authorityGroup.getDescription())
+                .type(authorityGroup.getType())
+                .build();
+        }
+    }
 
     @Data
     @NoArgsConstructor
@@ -23,13 +50,21 @@ public class AuthorityGroupReadDto {
 
         private AuthorityGroupType type;
 
-        public static Response fromEntity(AuthorityGroup authorityGroup){
-            return Response.builder()
-                    .authorityGroupId(authorityGroup.getAuthorityGroupId())
-                    .name(authorityGroup.getName())
-                    .description(authorityGroup.getDescription())
-                    .type(authorityGroup.getType())
-                    .build();
+        private List<AuthorityReadDto.Response> authorities;
+
+        public static Response fromEntity(AuthorityGroup authorityGroup, List<Authority> authorities){
+            Response.ResponseBuilder builder = Response.builder();
+
+            builder.authorities(authorities.stream().map(
+                AuthorityReadDto.Response::fromEntity).collect(
+                Collectors.toList()));
+
+            return builder
+                .authorityGroupId(authorityGroup.getAuthorityGroupId())
+                .name(authorityGroup.getName())
+                .description(authorityGroup.getDescription())
+                .type(authorityGroup.getType())
+                .build();
         }
     }
 }
