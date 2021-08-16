410787f0042606a52c83280739f17f28dbbd72af
dubu4050 <dubu4050@naver.com>
2021-08-16 16:09:56 +0900

    Feat: 공통 환경 수정
    - Response NoArgsConstructor 추가
    - MultipartFileService의 service명 받아올 수 있도록 @value 이용하여 SERVICE_NAME추가
    - 기존 new 이용한 RestTemplate을 Bean으로 관리하도록 Config 추가(세부 설정은 추가 논의 필요)
    
    # KIT-122

---
+++ src/main/java/com/se/apiserver/v1/common/infra/resttemplate/config/RestTemplateConfig.java
@@ -0,0 +1,30 @@
+package com.se.apiserver.v1.common.infra.resttemplate.config;
+
+import java.util.concurrent.TimeUnit;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+  @Bean
+  public RestTemplate restTemplate(){
+    HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+
+    HttpClient client = HttpClientBuilder.create()
+        .setMaxConnTotal(100)
+        .setMaxConnPerRoute(50)
+        .setConnectionTimeToLive(5, TimeUnit.SECONDS)
+        .build();
+
+    factory.setHttpClient(client);
+    factory.setConnectTimeout(3000);
+    factory.setReadTimeout(5000);
+
+    return new RestTemplate(factory);
+  }
+}
