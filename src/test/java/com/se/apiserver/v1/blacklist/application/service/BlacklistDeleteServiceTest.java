b8b591b3b04647e053db3db996d113eb55ea02e1
dubu4050 <dubu4050@naver.com>
2021-08-24 21:22:50 +0900

    Test: BlacklistDeleteService unit test
    - BlacklistDeleteService unit test 추가
    
    # KIT-159

---
+++ src/test/java/com/se/apiserver/v1/blacklist/application/service/BlacklistDeleteServiceTest.java
@@ -0,0 +1,47 @@
+package com.se.apiserver.v1.blacklist.application.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.se.apiserver.v1.blacklist.application.error.BlacklistErrorCode;
+import com.se.apiserver.v1.blacklist.domain.entity.Blacklist;
+import com.se.apiserver.v1.blacklist.infra.repository.BlacklistJpaRepository;
+import com.se.apiserver.v1.common.domain.exception.BusinessException;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class BlacklistDeleteServiceTest {
+
+  @Mock
+  private BlacklistJpaRepository blacklistJpaRepository;
+
+  @InjectMocks
+  private BlacklistDeleteService blacklistDeleteService;
+
+  @Test
+  public void 블랙리스트_삭제_성공() throws Exception{
+    //given
+    long id = 0L;
+    when(blacklistJpaRepository.findById(anyLong())).thenReturn(Optional.of(mock(Blacklist.class)));
+    //when
+    //then
+    assertDoesNotThrow(() -> blacklistDeleteService.delete(id));
+  }
+
+  @Test
+  public void 블랙리스트_삭제_실패_ID_불일치() throws Exception{
+    //given
+    long id = 0L;
+    //when
+    BusinessException exception = assertThrows(BusinessException.class, () -> blacklistDeleteService.delete(id));
+    //then
+    assertEquals(BlacklistErrorCode.NO_SUCH_BLACKLIST, exception.getErrorCode());
+  }
+}
