20addf53e0c29fade82e32ad8cb5e9e4cd5f10d1
dubu4050 <dubu4050@naver.com>
2021-08-24 21:35:53 +0900

    Fix: lombok 수정
    - @Data -> @Getter
    - @AllArgs 삭제
    
    # KIT-159

--- src/main/java/com/se/apiserver/v1/blacklist/application/dto/BlacklistCreateDto.java
+++ src/main/java/com/se/apiserver/v1/blacklist/application/dto/BlacklistCreateDto.java
@@ -2,18 +2,16 @@ package com.se.apiserver.v1.blacklist.application.dto;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Size;
 
 public class BlacklistCreateDto {
     @NoArgsConstructor
-    @AllArgsConstructor
     @Builder
-    @Data
+    @Getter
     @ApiModel("블랙리스트 등록 요청")
     static public class Request{
         @ApiModelProperty(notes = "아이피", example = "127.0.0.1")
@@ -23,5 +21,10 @@ public class BlacklistCreateDto {
         @ApiModelProperty(notes = "사유", example = "광고성 댓글")
         @Size(min = 4, max = 20)
         String reason;
+
+        public Request(@Size(min = 4, max = 20) String ip, @Size(min = 4, max = 20) String reason) {
+            this.ip = ip;
+            this.reason = reason;
+        }
     }
 }
--- src/main/java/com/se/apiserver/v1/blacklist/application/dto/BlacklistReadDto.java
+++ src/main/java/com/se/apiserver/v1/blacklist/application/dto/BlacklistReadDto.java
@@ -3,16 +3,14 @@ package com.se.apiserver.v1.blacklist.application.dto;
 import com.se.apiserver.v1.blacklist.domain.entity.Blacklist;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 public class BlacklistReadDto {
     @NoArgsConstructor
-    @AllArgsConstructor
     @Builder
-    @Data
+    @Getter
     @ApiModel("블랙리스트 조회 응답")
     static public class Response{
         @ApiModelProperty(notes = "블랙리스트 아이디", example = "1")
@@ -23,6 +21,12 @@ public class BlacklistReadDto {
 
         @ApiModelProperty(notes = "사유", example = "광고성 댓글")
         String reason;
+
+        public Response(Long id, String ip, String reason) {
+            this.id = id;
+            this.ip = ip;
+            this.reason = reason;
+        }
 
         public static Response fromEntity(Blacklist blacklist) {
             return Response.builder()
