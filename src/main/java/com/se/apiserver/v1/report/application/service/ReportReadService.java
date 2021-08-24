e43f5ddc7a3247743ba2aed336d65a59820e88d7
leejiwoo <tojue123@gmail.com>
2021-08-24 17:37:49 +0900

    관리자가 처음 읽었을 때 '읽음' 처리

--- src/main/java/com/se/apiserver/v1/report/application/service/ReportReadService.java
+++ src/main/java/com/se/apiserver/v1/report/application/service/ReportReadService.java
@@ -5,10 +5,10 @@ import com.se.apiserver.v1.report.application.dto.ReportReadDto;
 import com.se.apiserver.v1.report.application.dto.ReportReadDto.Response;
 import com.se.apiserver.v1.report.application.error.ReportErrorCode;
 import com.se.apiserver.v1.report.domain.entity.Report;
+import com.se.apiserver.v1.report.domain.entity.ReportStatus;
 import com.se.apiserver.v1.report.infra.repository.ReportJpaRepository;
 import java.util.List;
 import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -16,16 +16,27 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ReportReadService {
 
   private final ReportJpaRepository reportJpaRepository;
 
+  public ReportReadService(
+      ReportJpaRepository reportJpaRepository) {
+    this.reportJpaRepository = reportJpaRepository;
+  }
+
+  @Transactional
   public ReportReadDto.Response read(Long id){
     Report report = reportJpaRepository
         .findById(id)
         .orElseThrow(() -> new BusinessException(ReportErrorCode.NO_SUCH_REPORT));
+
+    if (report.getReportStatus() == ReportStatus.SUBMITTED) {
+      report.updateReportStatus(ReportStatus.REVIEWING);
+      reportJpaRepository.save(report);
+    }
+
     return ReportReadDto.Response.fromEntity(report);
   }
 
@@ -33,7 +44,7 @@ public class ReportReadService {
     Page<Report> all = reportJpaRepository.findAll(pageable);
     List<Response> responseList = all
         .stream()
-        .map(rl -> ReportReadDto.Response.fromEntity(rl))
+        .map(response -> ReportReadDto.Response.fromEntity(response))
         .collect(Collectors.toList());
     return new PageImpl(responseList, all.getPageable(), all.getTotalElements());
   }
