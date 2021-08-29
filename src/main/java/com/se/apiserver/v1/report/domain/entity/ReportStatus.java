cdbe1ad15727d06e15fb4e0ab95a30be4181bbf8
juegonjue <tojue123@gmail.com>
2021-08-29 17:40:03 +0900

    ReportStatus의 처리완료 (반려/승인) 에서 하나로 통합 및 ReportResult에 '반려'추가

--- src/main/java/com/se/apiserver/v1/report/domain/entity/ReportStatus.java
+++ src/main/java/com/se/apiserver/v1/report/domain/entity/ReportStatus.java
@@ -3,6 +3,5 @@ package com.se.apiserver.v1.report.domain.entity;
 public enum ReportStatus {
   SUBMITTED,  // 제출(처리전)
   REVIEWING,  // 읽음(처리중)
-  REJECTED,   // 반려(처리완료 -> ReportResult = null)
-  ACCEPTED,   // 승인(처리완료 -> ReportResult = has value)
+  COMPLETED    // 처리됨(처리완료)
 }
