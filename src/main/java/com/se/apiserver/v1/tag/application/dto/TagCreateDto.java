e18d4f06a7eeb36fe861688c20d4b12e6361a600
jduckling1024 <jduck1024@naver.com>
2021-08-29 21:26:01 +0900

    Entity 및 Dto 수정
    - 최대 태그 길이 수정
    - 롬복 수정

--- src/main/java/com/se/apiserver/v1/tag/application/dto/TagCreateDto.java
+++ src/main/java/com/se/apiserver/v1/tag/application/dto/TagCreateDto.java
@@ -5,22 +5,26 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 public class TagCreateDto {
-    @Data
+    @Getter
     @NoArgsConstructor
-    @AllArgsConstructor
     @Builder
     @ApiModel("태그 등록 요청")
     static public class Request{
         @ApiModelProperty(notes = "태그", value = "학사")
-        @Size(min = 2, max = 20)
+        @Size(min = 1, max = 20)
         @NotEmpty
         private String text;
+
+        public Request(@NotEmpty @Size(min = 1, max = 20) String text) {
+            this.text = text;
+        }
     }
 
     @Data
--- src/main/java/com/se/apiserver/v1/tag/application/service/TagCreateService.java
+++ src/main/java/com/se/apiserver/v1/tag/application/service/TagCreateService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class TagCreateService {
 
   private final TagJpaRepository tagJpaRepository;
