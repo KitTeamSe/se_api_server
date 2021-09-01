b4e74b8c70f8a440d898991448252b0b1906b7a4
jduckling1024 <jduck1024@naver.com>
2021-09-01 14:19:45 +0900

    수신 태그 등록 개수 제한

--- src/main/java/com/se/apiserver/v1/taglistening/application/service/TagListeningCreateService.java
+++ src/main/java/com/se/apiserver/v1/taglistening/application/service/TagListeningCreateService.java
@@ -3,6 +3,7 @@ package com.se.apiserver.v1.taglistening.application.service;
 import com.se.apiserver.v1.account.application.service.AccountContextService;
 import com.se.apiserver.v1.account.domain.entity.Account;
 import com.se.apiserver.v1.common.domain.exception.NotFoundException;
+import com.se.apiserver.v1.common.domain.exception.PreconditionFailedException;
 import com.se.apiserver.v1.common.domain.exception.UnauthenticatedException;
 import com.se.apiserver.v1.common.domain.exception.UniqueValueAlreadyExistsException;
 import com.se.apiserver.v1.tag.domain.entity.Tag;
@@ -10,8 +11,10 @@ import com.se.apiserver.v1.tag.infra.repository.TagJpaRepository;
 import com.se.apiserver.v1.taglistening.application.dto.TagListeningCreateDto;
 import com.se.apiserver.v1.taglistening.domain.entity.TagListening;
 import com.se.apiserver.v1.taglistening.infra.repository.TagListeningJpaRepository;
+import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import springfox.documentation.service.Tags;
 
 @Service
 @Transactional(readOnly = true)
@@ -40,6 +43,7 @@ public class TagListeningCreateService {
 
         Account account = accountContextService.getContextAccount();
 
+        checkPossibleToCreateTagListening(account.getAccountId());
         validateDuplicateTagListening(account, tag);
 
         TagListening tagListening = new TagListening(account,tag);
@@ -52,4 +56,11 @@ public class TagListeningCreateService {
             throw new UniqueValueAlreadyExistsException("중복된 내용 삽입");
     }
 
+    private void checkPossibleToCreateTagListening (Long accountId) {
+        List<TagListening> tagListenings = tagListeningJpaRepository.findAllByAccountId(accountId);
+
+        if (tagListenings.size() >= TagListening.MAX_TAG_LISTENING_CAPACITY) {
+            throw new PreconditionFailedException("등록 가능한 최대 수신 태그 개수를 초과하였습니다");
+        }
+    }
 }
--- src/test/java/com/se/apiserver/v1/taglistening/application/service/TagListeningCreateServiceTest.java
+++ src/test/java/com/se/apiserver/v1/taglistening/application/service/TagListeningCreateServiceTest.java
@@ -11,6 +11,7 @@ import com.se.apiserver.v1.account.domain.entity.Account;
 import com.se.apiserver.v1.account.domain.entity.AccountType;
 import com.se.apiserver.v1.account.domain.entity.InformationOpenAgree;
 import com.se.apiserver.v1.common.domain.exception.NotFoundException;
+import com.se.apiserver.v1.common.domain.exception.PreconditionFailedException;
 import com.se.apiserver.v1.common.domain.exception.UnauthenticatedException;
 import com.se.apiserver.v1.common.domain.exception.UniqueValueAlreadyExistsException;
 import com.se.apiserver.v1.tag.domain.entity.Tag;
@@ -19,6 +20,8 @@ import com.se.apiserver.v1.taglistening.application.dto.TagListeningCreateDto;
 import com.se.apiserver.v1.taglistening.application.dto.TagListeningCreateDto.Request;
 import com.se.apiserver.v1.taglistening.domain.entity.TagListening;
 import com.se.apiserver.v1.taglistening.infra.repository.TagListeningJpaRepository;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -149,5 +152,46 @@ public class TagListeningCreateServiceTest {
 
     // then
     assertThat(uniqueValueAlreadyExistsException.getMessage(), is("중복된 내용 삽입"));
+  }
+
+  @Test
+  void 등록_가능한_최대_수신_태그_개수_초과() {
+    // given
+    TagListeningCreateDto.Request request = Request
+        .builder()
+        .tagId(1L)
+        .build();
+    Tag tag = new Tag("태그");
+    Account account = new Account(1L
+        , "idString"
+        , "password"
+        , "name"
+        , "nickName"
+        , "studentId"
+        , AccountType.ASSISTANT
+        , "phoneNumber"
+        , "email@kumoh.ac.kr"
+        , "lastSignInIp"
+        , InformationOpenAgree.AGREE
+        , null
+        , null);
+
+    List<TagListening> tagListenings = new ArrayList<>();
+
+    for (int i = 0; i < TagListening.MAX_TAG_LISTENING_CAPACITY; i++) {
+      tagListenings.add(new TagListening(account, tag));
+    }
+
+    given(tagJpaRepository.findById(request.getTagId())).willReturn(java.util.Optional.of(tag));
+    given(accountContextService.isSignIn()).willReturn(true);
+    given(accountContextService.getContextAccount()).willReturn(account);
+    given(tagListeningJpaRepository.findAllByAccountId(account.getAccountId())).willReturn(tagListenings);
+
+    // when
+    PreconditionFailedException preconditionFailedException
+        = assertThrows(PreconditionFailedException.class, () -> tagListeningCreateService.create(request));
+
+    // then
+    assertThat(preconditionFailedException.getMessage(), is("등록 가능한 최대 수신 태그 개수를 초과하였습니다"));
   }
 }
