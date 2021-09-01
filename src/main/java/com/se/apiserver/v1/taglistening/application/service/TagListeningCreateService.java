53030e05c9c0a898ac224048aff268733ca42d0c
jduckling1024 <jduck1024@naver.com>
2021-09-01 13:06:46 +0900

    일부 Lombok 제거

--- src/main/java/com/se/apiserver/v1/taglistening/application/service/TagListeningCreateService.java
+++ src/main/java/com/se/apiserver/v1/taglistening/application/service/TagListeningCreateService.java
@@ -3,6 +3,9 @@ package com.se.apiserver.v1.taglistening.application.service;
 import com.se.apiserver.v1.account.application.service.AccountContextService;
 import com.se.apiserver.v1.account.domain.entity.Account;
 import com.se.apiserver.v1.common.domain.exception.BusinessException;
+import com.se.apiserver.v1.common.domain.exception.NotFoundException;
+import com.se.apiserver.v1.common.domain.exception.UnauthenticatedException;
+import com.se.apiserver.v1.common.domain.exception.UniqueValueAlreadyExistsException;
 import com.se.apiserver.v1.tag.domain.entity.Tag;
 import com.se.apiserver.v1.taglistening.domain.entity.TagListening;
 import com.se.apiserver.v1.tag.application.error.TagErrorCode;
@@ -10,22 +13,34 @@ import com.se.apiserver.v1.tag.application.error.TagListeningErrorCode;
 import com.se.apiserver.v1.taglistening.application.dto.TagListeningCreateDto;
 import com.se.apiserver.v1.tag.infra.repository.TagJpaRepository;
 import com.se.apiserver.v1.taglistening.infra.repository.TagListeningJpaRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
-@RequiredArgsConstructor
 public class TagListeningCreateService {
     private final TagListeningJpaRepository tagListeningJpaRepository;
     private final TagJpaRepository tagJpaRepository;
     private final AccountContextService accountContextService;
 
+    public TagListeningCreateService(
+        TagListeningJpaRepository tagListeningJpaRepository,
+        TagJpaRepository tagJpaRepository,
+        AccountContextService accountContextService) {
+        this.tagListeningJpaRepository = tagListeningJpaRepository;
+        this.tagJpaRepository = tagJpaRepository;
+        this.accountContextService = accountContextService;
+    }
+
     @Transactional
     public Long create(TagListeningCreateDto.Request request){
         Tag tag = tagJpaRepository.findById(request.getTagId())
-                .orElseThrow(() -> new BusinessException(TagErrorCode.NO_SUCH_TAG));
+                .orElseThrow(() -> new NotFoundException("존재하지 않는 태그"));
+
+        if (!accountContextService.isSignIn()) {
+            throw new UnauthenticatedException("수신 태그는 로그인 후 등록 가능합니다");
+        }
+
         Account account = accountContextService.getContextAccount();
 
         validateDuplicateTagListening(account, tag);
@@ -37,7 +52,7 @@ public class TagListeningCreateService {
 
     private void validateDuplicateTagListening(Account account, Tag tag) {
         if(tagListeningJpaRepository.findByAccountIdAndTagId(account.getAccountId(), tag.getTagId()).isPresent())
-            throw new BusinessException(TagListeningErrorCode.DUPLICATED);
+            throw new UniqueValueAlreadyExistsException("중복된 내용 삽입");
     }
 
 }
