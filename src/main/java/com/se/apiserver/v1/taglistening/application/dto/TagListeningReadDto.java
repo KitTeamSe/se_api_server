431d6e517c778fa3869cda60e160dc28092ea0e5
jduckling1024 <jduck1024@naver.com>
2021-09-01 20:30:28 +0900

    롬복 제거

--- src/main/java/com/se/apiserver/v1/taglistening/application/dto/TagListeningReadDto.java
+++ src/main/java/com/se/apiserver/v1/taglistening/application/dto/TagListeningReadDto.java
@@ -1,37 +1,44 @@
 package com.se.apiserver.v1.taglistening.application.dto;
 
 import com.se.apiserver.v1.taglistening.domain.entity.TagListening;
-import lombok.AllArgsConstructor;
+import javax.validation.constraints.Min;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Min;
-
 public class TagListeningReadDto {
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Builder
-    static public class Response{
 
-        private Long tagListeningId;
+  @Getter
+  @NoArgsConstructor
+  @Builder
+  static public class Response {
 
-        @Min(1)
-        private Long tagId;
-        private String tagName;
-        @Min(1)
-        private Long accountId;
-        private String accountIdString;
+    private Long tagListeningId;
 
-        static public Response fromEntity(TagListening tagListening){
-            return Response.builder()
-                    .accountId(tagListening.getAccount().getAccountId())
-                    .accountIdString(tagListening.getAccount().getIdString())
-                    .tagId(tagListening.getTag().getTagId())
-                    .tagName(tagListening.getTag().getText())
-                    .tagListeningId(tagListening.getTagListeningId())
-                    .build();
-        }
+    @Min(1)
+    private Long tagId;
+    private String tagName;
+    @Min(1)
+    private Long accountId;
+    private String accountIdString;
+
+    public Response(Long tagListeningId, Long tagId, String tagName, Long accountId,
+        String accountIdString) {
+      this.tagListeningId = tagListeningId;
+      this.tagId = tagId;
+      this.tagName = tagName;
+      this.accountId = accountId;
+      this.accountIdString = accountIdString;
     }
+
+    static public Response fromEntity(TagListening tagListening) {
+      return Response.builder()
+          .accountId(tagListening.getAccount().getAccountId())
+          .accountIdString(tagListening.getAccount().getIdString())
+          .tagId(tagListening.getTag().getTagId())
+          .tagName(tagListening.getTag().getText())
+          .tagListeningId(tagListening.getTagListeningId())
+          .build();
+    }
+  }
 }
--- src/main/java/com/se/apiserver/v1/taglistening/application/service/TagListeningReadService.java
+++ src/main/java/com/se/apiserver/v1/taglistening/application/service/TagListeningReadService.java
@@ -1,12 +1,13 @@
 package com.se.apiserver.v1.taglistening.application.service;
 
 import com.se.apiserver.v1.account.application.service.AccountContextService;
-import com.se.apiserver.v1.common.domain.exception.BusinessException;
-import com.se.apiserver.v1.taglistening.domain.entity.TagListening;
-import com.se.apiserver.v1.tag.application.error.TagListeningErrorCode;
+import com.se.apiserver.v1.common.domain.exception.NotFoundException;
 import com.se.apiserver.v1.taglistening.application.dto.TagListeningReadDto;
+import com.se.apiserver.v1.taglistening.application.dto.TagListeningReadDto.Response;
+import com.se.apiserver.v1.taglistening.domain.entity.TagListening;
 import com.se.apiserver.v1.taglistening.infra.repository.TagListeningJpaRepository;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -14,19 +15,22 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 @Transactional(readOnly = true)
-@RequiredArgsConstructor
 public class TagListeningReadService {
     private final TagListeningJpaRepository tagListeningJpaRepository;
     private final AccountContextService accountContextService;
 
+    public TagListeningReadService(
+        TagListeningJpaRepository tagListeningJpaRepository,
+        AccountContextService accountContextService) {
+        this.tagListeningJpaRepository = tagListeningJpaRepository;
+        this.accountContextService = accountContextService;
+    }
+
     public TagListeningReadDto.Response read(Long id){
         TagListening tagListening = tagListeningJpaRepository.findById(id)
-                .orElseThrow(() -> new BusinessException(TagListeningErrorCode.NO_SUCH_TAG_TAG_LISTENING));
+                .orElseThrow(() -> new NotFoundException("존재하지 않는 수신 태그"));
 
         if(!accountContextService.isOwner(tagListening.getAccount()) && !accountContextService.hasAuthority("TAG_MANAGE"))
             throw new AccessDeniedException("권한 없음");
@@ -34,18 +38,17 @@ public class TagListeningReadService {
         return TagListeningReadDto.Response.fromEntity(tagListening);
     }
 
-    public PageImpl readAllByPaging(Pageable pageable){
+    public Page<TagListeningReadDto.Response> readAllByPaging(Pageable pageable){
         Page<TagListening> all = tagListeningJpaRepository.findAll(pageable);
         List<TagListeningReadDto.Response> responseList = all.stream()
-                .map(t -> TagListeningReadDto.Response.fromEntity(t))
+                .map(Response::fromEntity)
                 .collect(Collectors.toList());
-
-        return new PageImpl(responseList, all.getPageable(), all.getTotalElements());
+        return new PageImpl<>(responseList, all.getPageable(), all.getTotalElements());
     }
 
     public List<TagListeningReadDto.Response> readByAccountId(Long accountId) {
         List<TagListening> all = tagListeningJpaRepository.findAllByAccountId(accountId);
-        return all.stream().map(a -> TagListeningReadDto.Response.fromEntity(a)).collect(Collectors.toList());
+        return all.stream().map(Response::fromEntity).collect(Collectors.toList());
     }
 
     public List<TagListeningReadDto.Response> readMy() {
