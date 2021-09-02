5cf2a700530ee36275cb18e0ba966c1e89d92bf5
dubu4050 <dubu4050@naver.com>
2021-09-02 16:07:59 +0900

    Fix: BlackListInterceptor 등록
    - 기존 Blacklist에 등록된 IP는 모두 차단하던 방식 -> readonly로 변경하도록 interceptor 등록
    
    # KIT-169

---
+++ src/main/java/com/se/apiserver/v1/common/infra/web/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.se.apiserver.v1.common.infra.web.config;
+
+import com.se.apiserver.v1.blacklist.application.service.BlacklistDetailService;
+import com.se.apiserver.v1.common.infra.web.interceptor.BlacklistInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.MappedInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  private final BlacklistDetailService blacklistDetailService;
+
+  public WebConfig(BlacklistDetailService blacklistDetailService) {
+    this.blacklistDetailService = blacklistDetailService;
+  }
+
+  @Bean
+  public MappedInterceptor blacklistInterceptor() {
+    String[] includePatterns = {"/api/v1/post/*", "/api/v1/reply/*"};
+    return new MappedInterceptor(includePatterns, new BlacklistInterceptor(blacklistDetailService));
+  }
+}
