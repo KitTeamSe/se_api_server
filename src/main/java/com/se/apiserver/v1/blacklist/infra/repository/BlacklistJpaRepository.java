8a2a940c2aba54505958b8b75945011ec9b2e57f
jueleejue <jueleejue@gmail.com>
2021-09-28 19:13:50 +0900

    fix : 블랙리스트 detail service및 interceptor 로직 수정

--- src/main/java/com/se/apiserver/v1/blacklist/infra/repository/BlacklistJpaRepository.java
+++ src/main/java/com/se/apiserver/v1/blacklist/infra/repository/BlacklistJpaRepository.java
@@ -1,14 +1,12 @@
 package com.se.apiserver.v1.blacklist.infra.repository;
 
+import com.se.apiserver.v1.account.domain.entity.Account;
 import com.se.apiserver.v1.blacklist.domain.entity.Blacklist;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface BlacklistJpaRepository extends JpaRepository<Blacklist, Long> {
-    Optional<Blacklist> findByIp(String ip);
-    Optional<Blacklist> findByIdString(String idString);
-    List<Blacklist> findAllByReleaseDateBefore(LocalDateTime today);
+    List<Blacklist> findByIpAndReleaseDateAfter(String ip, LocalDateTime today);
+    List<Blacklist> findByAccountAndReleaseDateAfter(Account account, LocalDateTime today);
 }
