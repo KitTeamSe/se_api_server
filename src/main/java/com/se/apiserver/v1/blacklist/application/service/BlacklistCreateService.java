44bd3f03d561628159cbc64f4276a0e99b2fb92f
jueleejue <jueleejue@gmail.com>
2021-09-28 19:22:18 +0900

    fix : 서비스 로직 수정

--- src/main/java/com/se/apiserver/v1/blacklist/application/service/BlacklistCreateService.java
+++ src/main/java/com/se/apiserver/v1/blacklist/application/service/BlacklistCreateService.java
@@ -1,5 +1,8 @@
 package com.se.apiserver.v1.blacklist.application.service;
 
+import com.se.apiserver.v1.account.application.error.AccountErrorCode;
+import com.se.apiserver.v1.account.domain.entity.Account;
+import com.se.apiserver.v1.account.infra.repository.AccountJpaRepository;
 import com.se.apiserver.v1.blacklist.domain.entity.Blacklist;
 import com.se.apiserver.v1.blacklist.application.error.BlacklistErrorCode;
 import com.se.apiserver.v1.blacklist.application.dto.BlacklistCreateDto;
@@ -15,23 +18,26 @@ import org.springframework.transaction.annotation.Transactional;
 public class BlacklistCreateService {
 
     private final BlacklistJpaRepository blacklistJpaRepository;
+    private final AccountJpaRepository accountJpaRepository;
 
     @Value("${spring.blacklist.release.default-plus-day}")
     private Integer DEFAULT_PLUS_DAY;
 
     public BlacklistCreateService(
-        BlacklistJpaRepository blacklistJpaRepository) {
+        BlacklistJpaRepository blacklistJpaRepository,
+        AccountJpaRepository accountJpaRepository) {
         this.blacklistJpaRepository = blacklistJpaRepository;
+        this.accountJpaRepository = accountJpaRepository;
     }
 
     @Transactional
     public Long create(BlacklistCreateDto.Request request) {
-        validateDuplicateIp(request.getIp());
-        validateDuplicateUser(request.getIdString());
+        Account account = accountJpaRepository.findById(request.getAccountId())
+            .orElseThrow(() -> new BusinessException(AccountErrorCode.NO_SUCH_ACCOUNT));
 
         Blacklist blacklist = new Blacklist(
             request.getIp(),
-            request.getIdString(),
+            account,
             request.getReason(),
             request.getReleaseDate() == null ? LocalDateTime.now().plusDays(DEFAULT_PLUS_DAY) : request.getReleaseDate()
         );
@@ -40,13 +46,4 @@ public class BlacklistCreateService {
         return blacklist.getBlacklistId();
     }
 
-    private void validateDuplicateIp(String ip) {
-        if(ip != null && blacklistJpaRepository.findByIp(ip).isPresent())
-            throw new BusinessException(BlacklistErrorCode.DUPLICATED_BLACKLIST);
-    }
-
-    private void validateDuplicateUser(String idString) {
-        if(idString != null && blacklistJpaRepository.findByIdString(idString).isPresent())
-            throw new BusinessException(BlacklistErrorCode.DUPLICATED_BLACKLIST);
-    }
 }
