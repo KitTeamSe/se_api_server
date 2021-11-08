33e7fa79f9fe1625b4c3bd88658ebd273ffd0b44
dldhk97 <dldhk97@gmail.com>
2021-11-08 15:56:14 +0900

    사용자 ID로 질문 조회 API 구현

--- src/test/java/com/se/apiserver/v1/account/application/service/QuestionReadServiceTest.java
+++ src/test/java/com/se/apiserver/v1/account/application/service/QuestionReadServiceTest.java
@@ -3,8 +3,10 @@ package com.se.apiserver.v1.account.application.service;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+import com.se.apiserver.v1.account.application.dto.QuestionReadDto;
 import com.se.apiserver.v1.account.domain.entity.Account;
 import com.se.apiserver.v1.account.domain.entity.Question;
+import com.se.apiserver.v1.account.infra.repository.AccountJpaRepository;
 import com.se.apiserver.v1.account.infra.repository.QuestionJpaRepository;
 import java.util.Collections;
 import org.junit.jupiter.api.Test;
@@ -20,12 +22,14 @@ class QuestionReadServiceTest {
   private AccountContextService accountContextService;
   @Mock
   private QuestionJpaRepository questionJpaRepository;
+  @Mock
+  private AccountJpaRepository accountJpaRepository;
   
   @InjectMocks
   private QuestionReadService questionReadService;
 
   @Test
-  public void 질문조회_성공_ALL() throws Exception{
+  public void 모든_질문목록_조회_성공() throws Exception{
     //given
     when(questionJpaRepository.findAll()).thenReturn(Collections.emptyList());
     //when
@@ -34,7 +38,7 @@ class QuestionReadServiceTest {
   }
 
   @Test
-  public void 질문조회_성공_MINE() throws Exception{
+  public void 내_질문조회_성공() throws Exception{
     //given
     when(accountContextService.getContextAccount()).thenReturn(
         Account.builder()
@@ -45,5 +49,19 @@ class QuestionReadServiceTest {
     //when
     //then
     assertDoesNotThrow(() -> questionReadService.readMyQuestionAndAnswer());
+  }
+
+  @Test
+  public void 사용자_아이디로_질문조회_성공() {
+    //given
+    String userId = "newUser00001";
+    Question question = Question.builder().text("내 고향은????").build();
+    Account account = Account.builder().idString(userId).question(question).build();
+
+    when(accountJpaRepository.findByIdString(userId)).thenReturn(java.util.Optional.ofNullable(account));
+    //when
+    QuestionReadDto.Response response = questionReadService.readQuestionByUserId(userId);
+    //then
+    assertEquals(response, QuestionReadDto.Response.fromEntity(question));
   }
 }
